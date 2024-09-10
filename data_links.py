709cfe73a2d3d8c23fd27889683c9b508c1a118a
Tobai24 <oluwatobiiyanuoluwa24@gmail.com>
2024-09-10 23:45:47 +0000

    added the old testament links

---
+++ data_links.py
@@ -0,0 +1,53 @@
+# Get the link for the bible books from youtube
+
+# Old Testament
+
+sum = "https://youtu.be/ALsluAKBZ-c?si=6k-0eTAQGsVq0WjE"
+genesis_part1 = "https://youtu.be/GQI72THyO5I?si=Pa3O9TUPeR2v57qd"
+genesis_part2 = "https://youtu.be/F4isSyennFo?si=QcopgxV7kmdgQpij"
+exodus_part1 = "https://youtu.be/jH_aojNJM3E?si=iGBqLrhJQR1yJwKc"
+exodus_part2 = "https://youtu.be/oNpTha80yyE?si=R8gpLW9Qtzy7YdLd"
+leventicus = "https://youtu.be/IJ-FekWUZzE?si=bvwKGbdPPrXSdSDB"
+numbers = "https://youtu.be/tp5MIrMZFqo?si=5k2QlTy37bFtuRCo"
+deteuronmy = "https://youtu.be/q5QEH9bH8AU?si=WIK3l2LskCaueiGd"
+joshua = "https://youtu.be/JqOqJlFF_eU?si=DrgAOi5Oie1odhxg"
+judges = "https://youtu.be/kOYy8iCfIJ4?si=JIRwHJdKQ8f7rZ0w"
+first_samuel = "https://youtu.be/kOYy8iCfIJ4?si=ApttcZ33RiJM1sg2"
+second_samuel = "https://youtu.be/YvoWDXNDJgs?si=jm-SGfan5wJhB05N"
+kings = "https://youtu.be/bVFW3wbi9pk?si=HTi3RFxfAiTowwzQ"
+isaiah_part1 = "https://youtu.be/d0A6Uchb1F8?si=S1YiEMdgwq2w6O6P"
+isaiah_part2 = "https://youtu.be/_TzdEPuqgQg?si=NuQ78-syZeDw_e56"
+jeremiah = "https://youtu.be/RSK36cHbrk0?si=H2NOZddzWmsE-2IL"
+ezekiel_part1 = "https://youtu.be/R-CIPu1nko8?si=xd28knt-IvWWUxQw"
+ezekiel_part2 = "https://youtu.be/SDeCWW_Bnyw?si=42ps5IUjXWxZ7ctr"
+hosea = "https://youtu.be/kE6SZ1ogOVU?si=9TD1Gqo89fnxQHcl"
+joel = "https://youtu.be/zQLazbgz90c?si=EG-yV9Hd8rEKuyYZ"
+amos = "https://youtu.be/mGgWaPGpGz4?si=k6byodG5sbBqSdWF"
+obadiah = "https://youtu.be/i4ogCrEoG5s?si=cc1RuBezKlT50s8Y"
+jonah = "https://youtu.be/dLIabZc0O4c?si=NmVo6g_AMxhcrW0p"
+micah = "https://youtu.be/MFEUEcylwLc?si=i2WANz7PccQPiTSz"
+nahum = "https://youtu.be/Y30DanA5EhU?si=6ltWI0hMe_KeoDJJ"
+habakkuk = "https://youtu.be/OPMaRqGJPUU?si=KQw5XN0ZXGcbuZgX"
+zephaniah = "https://youtu.be/oFZknKPNvz8?si=YixbGCCiaBlkDYzQ"
+haggai = "https://youtu.be/juPvv_xcX-U?si=BeCAHZsingpa9KSX"
+zechariah = "https://youtu.be/_106IfO6Kc0?si=Bn_z3x6lDbERD-fY"
+malachi = "https://youtu.be/HPGShWZ4Jvk?si=6mlsvUtyHnLPKgl8"
+pslams = "https://youtu.be/j9phNEaPrv8?si=n2yCIX1FskuM7SlD"
+proverb = "https://youtu.be/AzmYV8GNAIM?si=eeoolXmemCdfvzK4"
+job = "https://youtu.be/xQwnH8th_fs?si=EOT71JM_f3IQqUWc"
+song_of_songs = "https://youtu.be/4KC7xE4fgOw?si=ijpcDZk7DJvrh5de"
+ruth = "https://youtu.be/0h1eoBeR4Jk?si=Y_1qAyTNWF2X8uf-"
+lamentations = "https://youtu.be/p8GDFPdaQZQ?si=yK_FR2j2SuphOAtW"
+eccelesiasis = "https://youtu.be/lrsQ1tc-2wk?si=fOT0TAhgvsT3EOCO"
+esther = "https://youtu.be/JydNSlufRIs?si=02HewRDINWKuiKLe"
+daniel = "https://youtu.be/9cSC9uobtPM?si=VNbN_iYL0SjMFJ7R"
+ezra_nehemiah = "https://youtu.be/MkETkRv9tG8?si=fHYzrEzbQxaAAtUc"
+chronicles = "https://youtu.be/HR7xaHv3Ias?si=cmrKqcvSYdVAITAD"
+
+wisdom_literature = {
+    proverb: "https://youtu.be/Gab04dPs_uA?si=8heULe65FoI-Y86V",
+    eccelesiasis: "https://youtu.be/VeUiuSK81-0?si=SYqZTgfPhQTYb06z",
+    job: "https://youtu.be/GswSg2ohqmA?si=odrS2g8pK9LEUkw1"
+}
+
+
